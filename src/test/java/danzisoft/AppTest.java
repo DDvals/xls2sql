72c09543f65d947fc893308bb99a2ed8c050eda0
ddvals <d.valsecchi88@gmail.com>
2020-04-18 16:48:43 +0200

    Cli input test

--- src/test/java/danzisoft/AppTest.java
+++ src/test/java/danzisoft/AppTest.java
@@ -2,6 +2,8 @@ package danzisoft;
 
 import static org.junit.Assert.assertTrue;
 
+import danzisoft.xls2sql.Xls2sql;
+import org.apache.commons.cli.*;
 import org.junit.Test;
 
 
@@ -17,6 +19,26 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testCliOptions() throws ParseException {
+        Options opt = Xls2sql.buildCliOptions();
+        String trimOpt = "-t";
+        String queryOpt = "-q";
+        String queryOptVal = "select * from test where col1 = [$0] and col2 = [$1];";
+        String inputOpt = "-i";
+        String inputOptVal = "test.xlsx";
+        String[] args = {trimOpt, queryOpt, queryOptVal, inputOpt, inputOptVal};
+
+        final CommandLineParser cliParser = new DefaultParser();
+        CommandLine cmd = cliParser.parse(opt, args);
+
+        assertTrue(cmd.hasOption("t"));
+        assertTrue(cmd.hasOption("q"));
+        assertTrue(cmd.hasOption("i"));
+        assertTrue(queryOptVal.equals(cmd.getOptionValue("q")));
+        assertTrue(inputOptVal.equals(cmd.getOptionValue("i")));
     }
     
 }
